4f6549d6cfdb747d9671c28c511bdc78845d89d0
Francesco Agnoletto <Francesco.agnoletto92@gmail.com>
2017-02-01 19:51:39 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,58 @@
+var express = require('express');
+var passport = require('passport');
+var routes = require('./routes/routes');
+var GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: 'http://localhost:3000/login/google/return'
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    // In this example, the user's Google profile is supplied as the user
+    // record.  In a production-quality application, the Google profile should
+    // be associated with a user record in the application's database, which
+    // allows for account linking and authentication with other identity
+    // providers.
+    return cb(null, profile);
+  }));
+
+
+// Configure Passport authenticated session persistence.
+//
+// In order to restore authentication state across HTTP requests, Passport needs
+// to serialize users into and deserialize users out of the session.  In a
+// production-quality application, this would typically be as simple as
+// supplying the user ID when serializing, and querying the user record by ID
+// from the database when deserializing.  However, due to the fact that this
+// example does not have a database, the complete Google profile is serialized
+// and deserialized.
+passport.serializeUser(function(user, cb) {
+  cb(null, user);
+});
+
+passport.deserializeUser(function(obj, cb) {
+  cb(null, obj);
+});
+
+var app = express();
+var port = process.env.PORT || 3000;
+
+// Configure view engine to render EJS templates.
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+// Use application-level middleware for common functionality, including
+// logging, parsing, and session handling.
+app.use(require('morgan')('combined'));
+app.use(require('cookie-parser')());
+app.use(require('body-parser').urlencoded({ extended: true }));
+app.use(require('express-session')({ secret: 'keyboard cat', resave: true, saveUninitialized: true }));
+
+// Initialize Passport and restore authentication state, if any, from the session. 
+app.use(passport.initialize());
+app.use(passport.session());
+
+routes(app);
+
+app.listen(port);
---
+++ routes/routes.js
@@ -0,0 +1,32 @@
+var passport = require('passport');
+
+module.exports = function(app) {
+    
+    app.use(require('body-parser').urlencoded({ extended: true }));
+
+    app.get('/',
+    function(req, res) {
+        res.render('home', { user: req.user });
+    });
+
+    app.get('/login',
+    function(req, res){
+        res.render('login');
+    });
+
+    app.get('/login/google',
+    passport.authenticate('google'));
+
+    app.get('/login/google/return', 
+    passport.authenticate('google', { failureRedirect: '/login' }),
+    function(req, res) {
+        res.redirect('/');
+    });
+
+    app.get('/profile',
+    require('connect-ensure-login').ensureLoggedIn(),
+    function(req, res){
+        res.render('profile', { user: req.user });
+    });
+
+}
