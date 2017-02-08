db2d71e640b182f0c6b36ec6df7e980d68682c61
Francesco Agnoletto <Francesco.agnoletto92@gmail.com>
2017-02-08 23:39:58 +0100

    add subtitle(?), image and better preview format fix

--- assets/script.js
+++ assets/script.js
@@ -2,14 +2,12 @@ $(document).ready(function(){
 
   $('#newArticleTextArea').on('change keyup paste', function() {
     $('#preview').html(
-      marked($('#newArticleTitle').val()),
-      marked($('#newArticleSubtitle').val()),
       marked(
         "<h1>"+$('#newArticleTitle').val()+"</h1>"+
         "<h2>"+$('#newArticleSubtitle').val()+"</h2>"+
         $('#newArticleTextArea').val()
       )
-      );
+    );
   });
 
 });
